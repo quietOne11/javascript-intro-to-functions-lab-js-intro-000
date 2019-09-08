f5046435bd6dc51ea1d0f86f6af8b0662b4efb45
kim <krmciver@mail.sfsu.edu>
2019-09-08 20:23:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function whisper(string) {
 function sayHiToGrandma(string) {
   if(string.toLowerCase()) {
     return "I can't hear you!"
-  } else if (string.toUpperCase) {
+  } else if (string.toUpperCase()) {
     return "YES INDEED!"
   }
 }
