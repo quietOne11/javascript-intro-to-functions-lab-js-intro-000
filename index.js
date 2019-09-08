b4d6d0a46da244daf913f420254e13d08d9cb764
kim <krmciver@mail.sfsu.edu>
2019-09-08 20:23:02 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function logShout(string) {
+  //return string.toUpperCase();
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  //return string.toUpperCase();
+  console.log(string.toLowerCase());
+}
+
+function shout(string) {
+  //return string.toUpperCase();
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function sayHiToGrandma(string) {
+  if(string.toLowerCase()) {
+    return "I can't hear you!"
+  } else if (string.toUpperCase) {
+    return "YES INDEED!"
+  }
+}
